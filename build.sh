13624bf4035ab39c7ff9123a72bed23d4d88aec5
webdev778 <webdev778@gmail.com>
2024-03-07 14:23:13 +0900

    [test]

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 rm -rf src/interscript/maps
 mkdir -p src/interscript/maps
 pushd ../ruby
+bundle config set --local without 'secryst jsexec'
 bundle install || exit 1
 bundle exec rake compile[python,../python/src/interscript/maps] || exit 1
 popd
